d09713d386ad82e2357f34d5a12659ce12afd4b0
ddasni <danielsousamelo5@gmail.com>
2025-03-14 20:13:59 -0300

    Modificações e organizando a estrutura

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,247 +1,252 @@
 <template>
-<body>
-  	
-  	<div class="footer">
-    		<div class="footer1">
-      			<div class="primary-footer">
-        				<img class="logo-icon" alt="" src="logo.png">
-        				
-        				<div class="grupo">
-          					<div class="informaes">
-            						<div class="sobre">
-              							<div class="texto-e-emoji">
-                								<img class="vector-icon" alt="" src="/assets/icons/interrogacao.svg">
-                								
-                								<div class="sobre1">Sobre</div>
-              							</div>
-            						</div>
-            						<div class="contato">
-              							<div class="icon-mail">
-                								<img class="vector-icon1" alt="" src="/assets/icons/email.svg">
-                								
-                								<div class="contato1">Contato</div>
-                								
-                								
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-      			</div>
-      			<div class="second-footer">
-        				//* linha do footer
-        				
-        				<div class="informaes-finais">
-          					<div class="icone">
-          					</div>
-          					<div class="promohawk-todos-os">© 2025 PROMOHAWK. TODOS OS DIREITOS RESERVADOS</div>
-          					<div class="social">
-            						<img class="\assets/icons/🦆 icon _github_.svg">
-            						
-          					</div>
-        				</div>
-      			</div>
-    		</div>
-  	</div>
-</body>
+  <body>
+    <div class="footer">
+      <div class="footer1">
+        <div class="primary-footer">
+          <img class="logo-icon" alt="" src="~/assets/images/Logo.png" />
+
+          <div class="grupo">
+            <div class="informaes">
+              <div class="sobre">
+                <div class="texto-e-emoji">
+                  <img
+                    class="vector-icon"
+                    alt=""
+                    src="/assets/icons/interrogacao.svg"
+                  />
+
+                  <div class="sobre1">Sobre</div>
+                </div>
+              </div>
+              <div class="contato">
+                <div class="icon-mail">
+                  <img
+                    class="vector-icon1"
+                    alt=""
+                    src="/assets/icons/email.svg"
+                  />
+
+                  <div class="contato1">Contato</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="second-footer">
+          <!--linha do footer--> 
+
+          <div class="informaes-finais">
+            <div class="icone"></div>
+            <div class="promohawk-todos-os">
+              © 2025 PROMOHAWK. TODOS OS DIREITOS RESERVADOS
+            </div>
+            <div class="social">
+              <img class="\assets/icons/🦆 icon _github_.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <style>
 .logo-icon {
-  	width: 660px;
-  	height: 190px;
-  	object-fit: cover;
+  width: 660px;
+  height: 190px;
+  object-fit: cover;
 }
 .vector-icon {
-  	width: 15px;
-  	position: relative;
-  	height: 15px;
+  width: 15px;
+  position: relative;
+  height: 15px;
 }
 .sobre1 {
-  	position: relative;
-  	font-weight: 600;
+  position: relative;
+  font-weight: 600;
 }
 .texto-e-emoji {
-  	width: 80px;
-  	height: 23px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 5px;
+  width: 80px;
+  height: 23px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
 }
 .sobre {
-  	width: 115px;
-  	border-radius: 20px;
-  	background-color: #fff;
-  	height: 30px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: flex-start;
-  	justify-content: flex-start;
-  	padding: 6px 20px;
-  	box-sizing: border-box;
+  width: 115px;
+  border-radius: 20px;
+  background-color: #fff;
+  height: 30px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 6px 20px;
+  box-sizing: border-box;
 }
 .vector-icon1 {
-  	width: 16px;
-  	position: relative;
-  	height: 16px;
-  	z-index: 0;
+  width: 16px;
+  position: relative;
+  height: 16px;
+  z-index: 0;
 }
 .contato1 {
-  	width: 58.7px;
-  	position: relative;
-  	font-weight: 600;
-  	display: inline-block;
-  	height: 16px;
-  	flex-shrink: 0;
-  	z-index: 1;
+  width: 58.7px;
+  position: relative;
+  font-weight: 600;
+  display: inline-block;
+  height: 16px;
+  flex-shrink: 0;
+  z-index: 1;
 }
 .vector-icon2 {
-  	width: 16.64%;
-  	position: absolute;
-  	margin: 0 !important;
-  	height: 66.52%;
-  	top: 16.67%;
-  	right: 81.7%;
-  	bottom: 16.81%;
-  	left: 1.67%;
-  	max-width: 100%;
-  	overflow: hidden;
-  	max-height: 100%;
-  	z-index: 2;
+  width: 16.64%;
+  position: absolute;
+  margin: 0 !important;
+  height: 66.52%;
+  top: 16.67%;
+  right: 81.7%;
+  bottom: 16.81%;
+  left: 1.67%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+  z-index: 2;
 }
 .icon-mail {
-  	width: 110px;
-  	height: 23px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: flex-start;
-  	position: relative;
-  	gap: 5px;
+  width: 110px;
+  height: 23px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  gap: 5px;
 }
 .contato {
-  	width: 155px;
-  	border-radius: 20px;
-  	background-color: #fff;
-  	height: 30px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: flex-start;
-  	justify-content: flex-start;
-  	padding: 6px 20px;
-  	box-sizing: border-box;
-  	text-align: center;
+  width: 155px;
+  border-radius: 20px;
+  background-color: #fff;
+  height: 30px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 6px 20px;
+  box-sizing: border-box;
+  text-align: center;
 }
 .informaes {
-  	position: absolute;
-  	top: 0px;
-  	left: 0px;
-  	width: 253px;
-  	height: 30px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: flex-start;
-  	justify-content: flex-start;
-  	gap: 10px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 253px;
+  height: 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 10px;
 }
 .grupo {
-  	width: 253px;
-  	position: relative;
-  	height: 30px;
+  width: 253px;
+  position: relative;
+  height: 30px;
 }
 .primary-footer {
-  	height: 230px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	display: flex;
-  	flex-direction: column;
-  	align-items: center;
-  	justify-content: flex-start;
+  height: 230px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
 }
 .diviso-icon {
-  	width: 1074px;
-  	position: relative;
-  	height: 2px;
-  	object-fit: contain;
-  	opacity: 0.1;
+  width: 1074px;
+  position: relative;
+  height: 2px;
+  object-fit: contain;
+  opacity: 0.1;
 }
 .icone {
-  	width: 110px;
-  	height: 30px;
-  	overflow: hidden;
-  	flex-shrink: 0;
+  width: 110px;
+  height: 30px;
+  overflow: hidden;
+  flex-shrink: 0;
 }
 .promohawk-todos-os {
-  	width: 371px;
-  	position: relative;
-  	line-height: 14px;
-  	display: inline-block;
-  	height: 14px;
-  	flex-shrink: 0;
+  width: 371px;
+  position: relative;
+  line-height: 14px;
+  display: inline-block;
+  height: 14px;
+  flex-shrink: 0;
 }
 .icon-github {
-  	width: 25px;
-  	position: relative;
-  	max-height: 100%;
+  width: 25px;
+  position: relative;
+  max-height: 100%;
 }
 .social {
-  	width: 110px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: flex-start;
-  	justify-content: flex-start;
+  width: 110px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
 }
 .informaes-finais {
-  	width: 1074px;
-  	height: 30px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: flex-start;
-  	gap: 240px;
+  width: 1074px;
+  height: 30px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 240px;
 }
 .second-footer {
-  	width: 1074px;
-  	height: 55px;
-  	display: flex;
-  	flex-direction: column;
-  	align-items: flex-start;
-  	justify-content: flex-start;
-  	gap: 10px;
-  	text-align: center;
-  	font-family: 'DM Sans';
+  width: 1074px;
+  height: 55px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 10px;
+  text-align: center;
+  font-family: "DM Sans";
 }
 .footer1 {
-  	position: absolute;
-  	top: 0px;
-  	left: 183px;
-  	width: 1074px;
-  	height: 300px;
-  	overflow: hidden;
-  	display: flex;
-  	flex-direction: column;
-  	align-items: center;
-  	justify-content: flex-start;
-  	gap: 15px;
+  position: absolute;
+  top: 0px;
+  left: 183px;
+  width: 1074px;
+  height: 300px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 15px;
 }
 .footer {
-  	width: 100%;
-  	position: relative;
-  	background-color: #fff;
-  	height: 300px;
-  	overflow: hidden;
-  	text-align: left;
-  	font-size: 14px;
-  	color: #737373;
-  	font-family: Inter;
+  width: 100%;
+  position: relative;
+  background-color: #fff;
+  height: 300px;
+  overflow: hidden;
+  text-align: left;
+  font-size: 14px;
+  color: #737373;
+  font-family: Inter;
 }
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,210 +1,164 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<body>
-<div class="header">
-<div class="header-v2">
-<div class="header-v5">
-<div class="container">
-<div class="pesquisar">
-<div class="searchbar">
-<div class="content">
-<div class="magnifyingglass">
-<div class="magnifyingglass1">􀊫</div>
-</div>
-<div class="placeholder-label">Search</div>
-</div>
-</div>
-</div>
-<img class="logo-icon" alt="" src="~/assets/images/Logo.png">
-<div class="nav-list">
-<div class="link">
-<div class="master-link">
-<div class="incio">Início</div>
-</div>
-</div>
-<div class="link">
-<div class="master-link">
-<div class="incio">Categorias</div>
-</div>
-</div>
-<div class="link">
-<div class="master-link">
-<div class="incio">Lojas</div>
-</div>
-</div>
-<div class="link">
-<div class="master-link">
-<div class="incio"><NuxtLink to="/Cupons">Cupons</NuxtLink></div>
-</div>
-</div>
-<img class="user-icon" alt="" src="~/assets/icons/User.svg">
-</div>
-</div>
-</div>
-<div class="header-v2-child">
-</div>
-<div class="header-v2-item">
-</div>
-</div>
-</div>
-</body>
+  <body>
+    <div class="header">
+      <div class="estrutura-do-header">
+        <div class="pesquisar">
+          <div class="sample">
+            <div class="item">
+              <div class="search">Search ...</div>
+            </div>
+            <div class="icon">
+              <img class="search-icon" alt="" src="~/assets/icons/search.svg" />
+            </div>
+          </div>
+        </div>
+        <img class="logo-icon" alt="" src="~/assets/images/Logo.png" />
+
+        <div class="nav-list">
+          <div class="link">
+            <div class="master-link">
+              <div class="incio">Início</div>
+            </div>
+          </div>
+          <div class="link">
+            <div class="master-link">
+              <div class="incio">Categorias</div>
+            </div>
+          </div>
+          <div class="link">
+            <div class="master-link">
+              <div class="incio">Lojas</div>
+            </div>
+          </div>
+          <div class="link">
+            <div class="master-link">
+              <div class="incio">Cupons</div>
+            </div>
+          </div>
+          <img class="user-icon" alt="" src="~/assets/icons/User.svg" />
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <style>
-.magnifyingglass1 {
-  	position: absolute;
-  	height: 100%;
-  	width: 100%;
-  	top: 0%;
-  	left: 0%;
-  	line-height: 50px;
-  	font-weight: 500;
-  	display: flex;
-  	align-items: center;
-  	justify-content: center;
+.search {
+  position: relative;
+  line-height: 24px;
 }
-.magnifyingglass {
-  	width: 28px;
-  	position: relative;
-  	height: 28px;
-  	overflow: hidden;
-  	flex-shrink: 0;
+.item {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px 10px;
 }
-.placeholder-label {
-  	flex: 1;
-  	position: relative;
-  	font-size: 17px;
-  	line-height: 23.8px;
-  	font-family: Roboto;
-  	text-align: left;
+.search-icon {
+  width: 24px;
+  position: relative;
+  height: 24px;
+  overflow: hidden;
+  flex-shrink: 0;
 }
-.content {
-  	align-self: stretch;
-  	border-radius: 10px;
-  	background-color: rgba(250, 250, 250, 0.93);
-  	height: 36px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: flex-start;
-  	padding: 8px;
-  	box-sizing: border-box;
+.icon {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 10px;
 }
-.searchbar {
-  	width: 400px;
-  	display: flex;
-  	flex-direction: column;
-  	align-items: flex-start;
-  	justify-content: flex-start;
+.sample {
+  width: 450px;
+  border-radius: 32px;
+  background-color: #eaeaea;
+  height: 60px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px 8px;
+  box-sizing: border-box;
+  gap: 16px;
 }
 .pesquisar {
-  	width: 400px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
+  width: 460px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 .logo-icon {
-  	width: 106px;
-  	position: relative;
-  	height: 106px;
-  	overflow: hidden;
-  	flex-shrink: 0;
-  	object-fit: cover;
+  width: 106px;
+  position: relative;
+  height: 106px;
+  overflow: hidden;
+  flex-shrink: 0;
+  object-fit: cover;
 }
 .incio {
-  	position: relative;
-  	line-height: 23.8px;
+  position: relative;
+  line-height: 23.8px;
 }
 .master-link {
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: flex-start;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
 }
 .link {
-  	display: flex;
-  	flex-direction: row;
-  	align-items: flex-start;
-  	justify-content: flex-start;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
 }
 .user-icon {
-  	width: 30px;
-  	position: relative;
-  	max-height: 100%;
+  width: 30px;
+  position: relative;
+  max-height: 100%;
 }
 .nav-list {
-  	width: 400px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 32px;
-  	font-size: 17px;
-  	color: #170f49;
-  	font-family: Roboto;
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  text-align: center;
+  font-size: 17px;
+  color: #170f49;
 }
-.container {
-  	position: absolute;
-  	top: 6.52px;
-  	left: 97.68px;
-  	width: 1240px;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	gap: 165px;
-}
-.header-v5 {
-  	position: absolute;
-  	top: 0px;
-  	left: 0.32px;
-  	background-color: #fff;
-  	width: 1439.7px;
-  	height: 118px;
-  	overflow: hidden;
-}
-.header-v2-child {
-  	position: absolute;
-  	top: -0.5px;
-  	left: -0.18px;
-  	border-top: 1px solid #eff0f6;
-  	box-sizing: border-box;
-  	width: 1440.7px;
-  	height: 1px;
-}
-.header-v2-item {
-  	position: absolute;
-  	top: 117.55px;
-  	left: -0.5px;
-  	border-top: 1px solid #d9dbe9;
-  	box-sizing: border-box;
-  	width: 1440.7px;
-  	height: 1px;
-}
-.header-v2 {
-  	width: 1440px;
-  	position: relative;
-  	height: 118px;
+.estrutura-do-header {
+  width: 1440px;
+  height: 115px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 116px;
+  box-sizing: border-box;
+  gap: 130px;
 }
 .header {
-  	width: 100%;
-  	position: relative;
-  	background-color: #fff;
-  	height: 115px;
-  	overflow: hidden;
-  	display: flex;
-  	flex-direction: row;
-  	align-items: center;
-  	justify-content: center;
-  	padding: 10px 160px;
-  	box-sizing: border-box;
-  	text-align: center;
-  	font-size: 18px;
-  	color: rgba(60, 60, 67, 0.6);
-  	font-family: 'SF Pro Display';
+  width: 100%;
+  position: relative;
+  background-color: #fff;
+  height: 115px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0px;
+  box-sizing: border-box;
+  text-align: left;
+  font-size: 23px;
+  color: #abb7c2;
+  font-family: Roboto;
 }
 </style>
