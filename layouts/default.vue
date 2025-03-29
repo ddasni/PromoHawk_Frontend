f1297a91e5dc1cf0cdd418073afb0157077d0f36
ddasni <danielsousamelo5@gmail.com>
2025-03-29 10:39:08 -0300

    Criando os layouts globais

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <Header />
+        <NuxtPage/>
+    <Footer />
+</template>
+
+<script setup>
+    import Header from '~/components/global/Header.vue';
+    import Footer from '~/components/global/Footer.vue';
+</script>
+
+<style>
+
+</style>
