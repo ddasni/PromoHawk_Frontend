9ce72684c035eb6cf796447101adcb940caec2ad
Promohawk <promohawk01@gmail.com>
2025-04-03 15:25:45 -0300

    testando um componente reciclavel

--- components/buttons/botao.vue
+++ components/buttons/botao.vue
@@ -1,11 +1,44 @@
-<script setup>
-
-</script>
-
 <template>
-    
+    <button :class="[BotaoClass, CorClass]"> {{ nome }} </button>
 </template>
 
-<style scoped>
+<script setup>
+    import { computed, toRefs } from 'vue'
 
+    const props = defineProps ({
+        nome: {
+            type: String,
+            default: 'Button',
+        },
+        size: {
+            type: String,
+            default: 'md',
+        },
+        cor: {
+            type: String,
+            default: 'primary',
+        },
+    })
+
+    const { nome, size, cor } = toRefs(props)
+
+    const BotaoClass = computed(() => {
+        return {
+            'px-2 py-1 text-sm': size.value === 'sm',
+            'px-4 py-2 text-md': size.value === 'md',
+            'px-3 py-3 text-lg': size.value === 'lg',
+        }
+    })
+
+    const CorClass = computed(() => {
+        return {
+            'bg-blue-500 text-white': cor.value === 'primary',
+            'bg-red-500 text-white': cor.value === 'dois',
+            'bg-green-500 text-white': cor.value === 'tres',
+        }
+    })
+</script>
+
+<style scoped>
+    
 </style>
---
+++ pages/testes.vue
@@ -0,0 +1,11 @@
+<template>
+  <Botao nome="teste" size="sm" color="tres"/>
+</template>
+
+<script setup>
+    import Botao from '~/components/buttons/botao.vue';
+</script>
+
+<style>
+
+</style>
