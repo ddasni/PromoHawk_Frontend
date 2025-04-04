4c9a291e21fd5ac62721ac51a8473c22402af381
ddasni <danielsousamelo5@gmail.com>
2025-04-03 23:07:15 -0300

    Inserindo um componente do NuxtUi

---
+++ components/Header/user_icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <UDropdownMenu
+    arrow
+    :items="items"
+    :ui="{
+      content: 'w-48'
+    }"
+  >
+    <UButton label="Open" icon="i-lucide-menu" color="neutral" variant="outline" />
+  </UDropdownMenu>
+</template>
+
+<script lang="ts" setup>
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: 'Profile',
+    icon: 'i-lucide-user'
+  },
+  {
+    label: 'Billing',
+    icon: 'i-lucide-credit-card'
+  },
+  {
+    label: 'Settings',
+    icon: 'i-lucide-cog'
+  }
+])
+</script>
+
+<style scoped>
+
+</style>
