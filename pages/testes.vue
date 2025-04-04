14101fe52f514bea491fa3584faba9dd74b80a6f
ddasni <danielsousamelo5@gmail.com>
2025-04-03 23:07:42 -0300

    testando o componente do NuxtUI

--- pages/testes.vue
+++ pages/testes.vue
@@ -1,11 +1,29 @@
 <template>
-  <Botao nome="teste" size="sm" color="tres"/>
+  <div class="container">
+    <Botao nome="teste" size="sm" color="tres"/>
+    <br><br>
+    <User />
+  </div>
 </template>
 
 <script setup>
-    import Botao from '~/components/buttons/botao.vue';
+definePageMeta({
+    layout: 'basic'
+})
+
+import Botao from '~/components/buttons/botao.vue';
+import User from '~/components/Header/user_icon.vue'
+
 </script>
 
-<style>
-
+<style scoped>
+.container {
+width: 90vw;
+height: 90vh;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+gap: 30px;
+}
 </style>
