a310a673014757acd1dcf3a0ad451d8fedbf3828
ddasni <danielsousamelo5@gmail.com>
2025-04-14 21:52:36 -0300

    Arrumando o Header

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="Header">
     <Nav/>
+
     <img src="/assets/images/Logo.png" alt="Logo">
-    <!-- Colcoar o componente search aqui -->
+    
+    <Search/>
     <User/>
   </div>
 </template>
 
 <script setup>
 import Nav from '~/components/Header/NavigationMenu.vue';
-import User from '~/components/Header/user_icon.vue'
+import Search from '../Header/Search.vue';
+import User from '~/components/Header/user_icon.vue';
 </script>
 
 <style scoped>
@@ -17,13 +20,17 @@ import User from '~/components/Header/user_icon.vue'
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px 160px;
-  background-color: transparent;
+  padding: 0px 160px;
+  background-color: blueviolet;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .Header img {
   height: 90px; /* altura da logo */
   object-fit: contain;
+}
+
+.estrutura {
+ width: 100px;
 }
 </style>
