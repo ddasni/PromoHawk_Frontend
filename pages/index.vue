823c77481041df1524e26053e9d2b4a766d7049e
Promohawk <promohawk01@gmail.com>
2025-04-16 15:16:33 -0300

    Adicionando os divisores na pagina index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  
+    <Divisor titulo="Categorias" actionTexto="ver mais >" link=""/> 
+    <br><br>
+    <Divisor titulo="Destaques" actionTexto="ver mais >" link=""/> 
+    <br><br>
+    <Divisor titulo="Cupons" actionTexto="ver mais >" link=""/> 
 </template>
+
+<script setup>
+    import Divisor from '~/components/Divisor.vue';
+</script>
+
+<style>
+
+</style>
