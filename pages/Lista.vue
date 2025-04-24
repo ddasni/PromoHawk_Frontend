8292e0e90f5b3ad12c1169ae6831a74cde88e974
Promohawk <promohawk01@gmail.com>
2025-04-24 15:05:44 -0300

    Alterando o nome e craindo uma nova pagina

---
+++ pages/Lista.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!--Aqui vai a lista das lojas ou Categorias. O conteudo vai mudar de acordo com o escolhido pelo usuario-->
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
