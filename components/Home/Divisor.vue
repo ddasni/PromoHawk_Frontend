70d1442f003ef450348f58a273b2bcf01c08457e
ddasni <danielsousamelo5@gmail.com>
2025-06-15 16:03:21 -0300

    adicionando um novo divisor de pagina

--- components/Home/Divisor.vue
+++ components/Home/Divisor.vue
@@ -1,78 +1,27 @@
 <template>
-    <div class="divisor">
-      <h2 class="titulo">{{ titulo }}</h2>
-  
-      <div class="linha" :class="[sizeClass]"></div>
-  
-      <NuxtLink :to="link" class="action">
-          {{ actionTexto }}
-      </NuxtLink>
-    </div>
+  <div class="flex flex-col sm:flex-row items-center justify-between gap-4 my-12">
+    <!-- Título -->
+    <h2 class="text-neutral-700 dark:text-neutral-500 text-lg font-semibold text-center sm:text-left">
+      {{ title }}
+    </h2>
+
+    <!-- Linha -->
+    <div class="hidden sm:flex flex-grow mx-4 h-px bg-gradient-to-r from-neutral-300 via-neutral-400 to-neutral-300 opacity-30 dark:via-neutral-500"></div>
+
+    <!-- Botão "Ver mais" -->
+    <NuxtLink
+      v-if="link"
+      :to="link"
+      class="relative inline-block text-sm font-medium text-neutral-400 hover:text-neutral-800 transition-colors before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-neutral-600 before:transition-all hover:before:w-full"
+    >
+      Ver mais
+    </NuxtLink>
+  </div>
 </template>
-  
-<script lang="ts" setup>
-  import { computed, toRefs } from 'vue'
-  
-  const props = withDefaults(
-    defineProps<{
-      titulo?: string
-      link?: string
-      actionTexto?: string
-      size?: 'sm' | 'md' | 'lg'
-    }>(),
-    {
-      // Definições padrão do botao
-      titulo: 'Escreva um titulo', // titulo padrão
-      size: 'md', // Tamanho padrão
-    }
-  )
-  
-  const { size } = toRefs(props)
-  
-  // classe dos Tamanhos do botao
-  const sizeClass = computed(() => {
-    switch (size.value) {
-      case 'sm':
-        return 'px-4 py-0.2 text-sm'
-      case 'lg':
-        return 'px-8 py-0.2 text-lg'
-      case 'md':
-      default:
-        return 'px-10 py-0.2 text-base'
-    }
-  })
+
+<script setup>
+defineProps({
+  title: String,
+  link: String
+})
 </script>
-  
-<style scoped>
-  .divisor {
-      display: flex;
-      position:absolut;
-      margin: 30px;
-      align-items: center;
-      gap: 0.5rem;
-      margin: 1.5rem 0;
-  }
-  
-  .titulo {
-      white-space: nowrap;
-      font-weight: bold;
-  }
-  
-  .linha {
-      flex: 1;
-      height: 1px;
-      width: 1000px;
-      background-color: #777777;
-  }
-  
-  .action {
-      white-space: nowrap;
-      font-size: 0.9rem;
-      color: #b6b6b6;
-      text-decoration: none;
-  }
-  
-  .action:hover {
-      text-decoration: underline;
-  }
-</style>
